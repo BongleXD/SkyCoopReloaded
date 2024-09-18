60f836896c61b1aec45f59888ba5e42943acf106
RED1cat <www.324132.com@gmail.com>
2024-09-18 23:12:35 +0500

    initialize reborn

---
+++ SkyCoopClient/ModMain.cs
@@ -0,0 +1,11 @@
+﻿using MelonLoader;
+
+namespace SkyCoop;
+internal sealed class ModMain : MelonMod
+{
+	public override void OnInitializeMelon()
+	{
+		//Any initialization code goes here.
+		//This method can be deleted if no initialization is required.
+	}
+}
---
+++ SkyCoopServer/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SkyCoopServer
+{
+    public class ServerMain
+    {
+
+    }
+}
